863290c5d34c1b3d970f7d175389c0d65daec0be
Juliusz Trochimczuk <assasindal@gmail.com>
2023-06-08 12:17:03 +0200

    Some cleaning in code; Now player can choose the reward

--- ConsoleTextRPG/ConsoleTextRPG/ConsoleTextRPG.cpp
+++ ConsoleTextRPG/ConsoleTextRPG/ConsoleTextRPG.cpp
@@ -9,18 +9,20 @@ using namespace std;
 
 void RewardingPlayer(Player& playerRef)
 {
-    int drawReward = GameController::GenerateNumber(1, 2);
-    if (drawReward == 1)
+    int choseReward = 0;
+    cout << "Choose your reward: 1 - Healing; 2 - Gold\n";
+    cin >> choseReward;
+    if (choseReward == 1)
     {
         int healingAmount = GameController::GenerateNumber(10, 25);
         cout << "Near the corpse you find the flask of life energy. It restoring " + to_string(healingAmount) + " of your health\n";
         playerRef.Heal(healingAmount);
         cout << "Now your healt amount is: " + to_string(playerRef.GetHealth()) << endl;
     }
-    else if (drawReward == 2)
+    else if (choseReward == 2)
     {
         int goldLooted = GameController::GenerateNumber(5, 15);
-        cout << "Near the corpse you find the pouch of gold. You decide to take it. Owner dosen't need it now anyway. You recive " + to_string(goldLooted) + " gold\n";
+        cout << "Near the corpse you find the sack of gold. You decide to take it. Owner dosen't need it now anyway. You recive " + to_string(goldLooted) + " gold\n";
         playerRef.Loot(goldLooted);
         cout << "Now you have " + to_string(playerRef.GetGold()) + " gold\n";
     }
@@ -30,6 +32,7 @@ void RewardingPlayer(Player& playerRef)
 void FightSystem(Player& playerRef, string oponentName)
 {
     Foe oponent = Foe(GameController::GetOponentData(oponentName));
+    cout << endl;
     while (playerRef.isDead != true && oponent.isDead != true)
     {
         int damageToOponent = playerRef.MakeAction();
@@ -51,6 +54,7 @@ int main()
    cout << "Hey Player! Welcome in magical world of Rivia. It's high fanatsy world with planty of stories, creaters and treasuer. But firstly you need to choose your character.\n";
    Warrior playerObject = Warrior();
    cout << "And here is your first creaters to kill. You encounter: Wolf\n";
+   FightSystem(playerObject, "Wolf");
    FightSystem(playerObject, "Wolf");
    cout << "Congratiulations on killing your first beast\n";
 }
